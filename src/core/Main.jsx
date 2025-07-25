5a3ed03d4105a1e75b9ab07c1db8e1f832021591
sathishtcz <msathish.tcz@gmail.com>
2025-07-25 14:13:14 +0530

    Sathish-master

--- src/core/Main.jsx
+++ src/core/Main.jsx
@@ -12,7 +12,7 @@ export default function Main() {
     return (
         <>
             <Header />
-            <main className={`w-auto mx-auto md:mt-5 mt-0 ${isHome ? 'lg:mt-[100px] 2xl:mt-[185px] ' : 'lg:mt-[110px]'}`}>
+            <main className={`w-auto mx-auto  md:mt-5 mt-0 ${isHome ? 'lg:mt-[100px] 2xl:mt-[185px] ' : 'lg:mt-[110px]'}`}>
                 <Outlet />
             </main>
             <Register />
